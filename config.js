7058c3ca0ee8c9a1e82f4888a96e358814612f1a
Abdul-Basit786 <ab627226@gmail.com>
2024-07-11 00:46:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ module.exports = {
   errorChat: process.env.ERROR_CHAT || "923457214057",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   antilink_values: process.env.ANTILINK_VALUES || "chat.whatsapp.com",
